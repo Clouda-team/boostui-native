6dd6c7c932ee3fa19c9d206241652bab7a7ce8e8
zhangyuanwei <zhangyuanwei1988@gmail.com>
2015-07-22 17:02:45 +0800

    优化json

--- boost/bridge.js
+++ boost/bridge.js
@@ -7,9 +7,8 @@ define(function (require, exports, module) {
     var assert = require("base/assert");
 
     var queue = genQueue(function (list) {
-        //list = mergeQueue(list);
-        console.log("callQueue(" + JSON.stringify(list, null, 2) + ")");
-        //console.log("callQueue(", list, ")");
+        //console.log("callQueue(" + JSON.stringify(list, null, 2) + ")");
+        console.log("callQueue(", list, ")");
         lc_bridge.callQueue(JSON.stringify(list));
 
         clearHeap();
@@ -29,6 +28,7 @@ define(function (require, exports, module) {
 
     var bridge = {
         call: function (tag, method, args) {
+            var cmd = {};
             var viewTag;
             var config;
             // 对createView、updateView 等做优化 
@@ -70,11 +70,19 @@ define(function (require, exports, module) {
                 }
             }
 
+            if (tag !== "") {
+                cmd.tag = tag;
+            }
+            cmd.method = method;
+            cmd.args = args;
+            queue.push(cmd);
+            /*
             queue.push({
                 tag: tag,
                 method: method,
                 args: args
             });
+             */
         }
     };
 
--- boost/tagMap.js
+++ boost/tagMap.js
@@ -1,37 +1,39 @@
-define(function (require, exports, module) {"use strict";
+define(function (require, exports, module) {
+    "use strict";
 
-var hasOwnProperty = require("base/hasOwnProperty");
+    var hasOwnProperty = require("base/hasOwnProperty");
 
-var tagMap = {};
-var tagId = 0;
+    var tagMap = {};
+    var tagId = 0;
 
-function get(tag) {
-    if (hasOwnProperty(tagMap, tag)) {
-        return tagMap[tag];
+    function get(tag) {
+        if (hasOwnProperty(tagMap, tag)) {
+            return tagMap[tag];
+        }
+        return null;
     }
-    return null;
-}
 
-function set(tag, obj) {
-    tagMap[tag] = obj;
-}
-
-function remove(tag) {
-    if (get(tag) !== null) {
-        tagMap[tag] = null;
+    function set(tag, obj) {
+        tagMap[tag] = obj;
     }
-    delete tagMap[tag];
-}
 
-function genTag() {
-    return "$__tag_" + tagId++ + "__$";
-}
+    function remove(tag) {
+        if (get(tag) !== null) {
+            tagMap[tag] = null;
+        }
+        delete tagMap[tag];
+    }
 
-module.exports = {
-    get: get,
-    set: set,
-    remove: remove,
-    genTag: genTag
-};
+    function genTag() {
+        //return "$__tag_" + tagId++ + "__$";
+        return "_" + tagId++;
+    }
+
+    module.exports = {
+        get: get,
+        set: set,
+        remove: remove,
+        genTag: genTag
+    };
 
 });

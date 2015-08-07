fd2718e92456e28b7fafb43956fc21b3b587af4e
zhangyuanwei <zhangyuanwei1988@gmail.com>
2015-08-07 17:09:25 +0800

    添加 Animation 事件

--- boost/Animation.js
+++ boost/Animation.js
@@ -6,12 +6,10 @@ define(function (require, exports, module) {
     var EventTarget = require("boost/EventTarget");
     var NativeObject = require("boost/NativeObject");
     var NativeElement = require("boost/NativeElement");
-    //var TouchEvent = require("boost/TouchEvent");
-    //var Element = require("boost/Element");
-    //
+    var AnimationEvent = require("boost/AnimationEvent");
+
     var nativeGlobal = NativeObject.global;
 
-    //var _super = Element.prototype;
     var Animation = derive(EventTarget, function (type, config) {
         EventTarget.call(this);
         this.__type__ = type;
@@ -38,13 +36,12 @@ define(function (require, exports, module) {
         __onEvent: function (type, e) {
             var event;
             switch (type) {
-                // TODO
-                //case "touchstart":
-                //case "touchend":
-                //event = new TouchEvent(this, type, e.x, e.y);
-                //this.dispatchEvent(event);
-                //break;
-                default: console.log("unknow event:" + type, e);
+            case "animationstart":
+            case "animationend":
+                event = new AnimationEvent(this, type);
+                break;
+            default:
+                console.log("unknow event:" + type, e);
             }
         }
     });
